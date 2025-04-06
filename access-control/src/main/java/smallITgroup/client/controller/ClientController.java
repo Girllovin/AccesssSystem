1099f33316796d6fd0f863e2fb6520080280c3a5
Dmitrii Builin <d.builin@mail.ru>
2025-04-06 22:08:11 +0300

    d.shtrikman - modified cardHolder.lava

--- access-control/src/main/java/smallITgroup/client/controller/ClientController.java
+++ access-control/src/main/java/smallITgroup/client/controller/ClientController.java
@@ -3,6 +3,7 @@ package smallITgroup.client.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,10 @@ import smallITgroup.client.dto.CardHolderDto;
 import smallITgroup.client.service.ClientService;
 
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+
+
 public class ClientController {
 	
 	final ClientService clientService;
